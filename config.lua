0a5df6cc172c45cbe1e784761680ddc127c98027
kubanscripts <130844381+kubanscripts@users.noreply.github.com>
2024-06-26 08:54:51 +1000

    Update config.lua
    - Added Config Option for All Icons
    - Added Config Option for Plate
    - Added Config Option for Notications & Progress Bar

--- config.lua
+++ config.lua
@@ -6,23 +6,28 @@ Config.Garages = {
     {
         job = "police",
         Locations = {
-            { coords = vector3(461.23, -980.37, 26.68), heading = 0.0 }
+            { coords = vector3(380.86, -1613.24, 29.99), heading = 0.0 }
         },
         CustomizationPoint = {
-            coords = vector3(451.2963, -976.3116, 25.6998),
+            coords = vector3(401.02, -1618.11, 29.29),
             heading = 0.0
         },
         DeletePoint = {
-            coords = vector3(459.2846, -992.4221, 26.2998),
+            coords = vector3(377.79, -1616.86, 29.29),
             heading = 0.0
+        },
+        SpawnPoint = {
+            coords = vector3(385.82, -1624.66, 29.29),
+            heading = 320.54
         }
     }
 }
 
 Config.Categories = {
     ['General Duties'] = {
-        { name = 'HSV', model = '14HSV' },
-        { name = 'Buffalo', model = 'buffalo' }
+        { name = 'Police', model = 'police' },
+        { name = 'Police Buffalo', model = 'police2' },
+        { name = 'Police Vapid', model = 'police3' }
     },
     ['Highway Patrol'] = {
         { name = 'Baller', model = 'baller' },
@@ -33,7 +38,7 @@ Config.Categories = {
         { name = 'Cavalcade', model = 'cavalcade' }
     },
     ['Criminal Investagation Branch'] = {
-        { name = 'Baller', model = 'baller' },
+        { name = 'Police Cruiser', model = 'police4' },
         { name = 'Cavalcade', model = 'cavalcade' }
     },
     ['Australian Federal Police'] = {
@@ -42,15 +47,17 @@ Config.Categories = {
     }
 }
 
-Config.SpawnPoint = {
-    coords = vector3(445.3, -986.22, 25.7),
-    heading = 262.85
-}
-
 Config.InteractKey = 38 
-Config.TextDrawDistance = 5.0 
+Config.TextDrawDistance = 4.0 
 Config.SpawnInVehicle = true
-Config.CategoryTitle = 'SAPF'
+Config.CategoryTitle = 'POLICE'
+Config.Plate = 'POLICE'
+
+--- Repair Settings
+Config.Timetorepair = 5000 -- 5 Seconds
+Config.ProgressBarText = "Repairing Vehicle..."
+Config.RepairSuccess = 'Vehicle repaired successfully.'
+Config.NotInVehicle = 'You are not in a vehicle.'
 
 -- Car Marker Settings
 Config.CarTextUI = 'Press ~g~[E]~w~ to open ~y~garage'
@@ -59,6 +66,12 @@ Config.CarDrawMarker = 36
 -- Customize Marker Settings
 Config.CustomTextUI = 'Press ~g~[E]~w~ to Customize ~y~Vehicle'
 Config.CustomDrawMarker = 27
+
+Config.RepairIcon = 'fas fa-wrench'
+Config.LiveryIcon = 'fas fa-paint-brush'
+Config.ExtraIcon = 'fas fa-tools'
+Config.ColorIcon = 'fas fa-palette'
+Config.PlateIcon = 'fas fa-id-card'
 
 -- Delete Marker Settings
 Config.DeleteTextUI = 'Press ~g~[E]~w~ to ~r~Delete ~w~Vehicle'
